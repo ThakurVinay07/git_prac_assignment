6139f09e420911dea761e1c3bdc428e64d44abd6
Vinay Kumar Thakur <002avithakur.07@gmail.com>
2022-12-09 18:03:16 +0530

    check

---
+++ pppp.js
@@ -0,0 +1,24 @@
+//   prime num check program
+
+
+let num=13
+
+let count=0;
+for(let i=1;i<=num;i++)
+{
+     if(num%j==0)
+    {
+      count++
+    }
+
+if(count==5)
+ {
+   console.log("Prime Number")
+}
+  else 
+{
+  console.log("Not Prime")
+}
+
+
+}
